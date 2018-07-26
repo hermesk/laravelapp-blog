630924e1f362e7bd76256fce87f1d6478eb4a631
hermesk <jimikinyua@gmail.com>
2018-07-26 20:43:01 +0300

    first commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    //
+    public function index(){
+        $title = 'Welcome to Laravel!';
+        //return view('pages.index',compact('title'));
+        return view('pages.index')->with('title',$title);
+    }
+    //about page
+    public function about(){
+        $title = 'About Us';
+        return view('pages.about')->with('title',$title);;
+    }
+      //services page
+      public function services(){
+        $data = array(
+            'title'  => 'Services',
+            'services'=> ['Programing','Web Design','DBMS']
+        );
+        
+        return view('pages.services')->with($data);;
+    }
+    
+
+}
